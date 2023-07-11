64725b4caad7ea977b9569e2ac5df6266cead21b
yoav uzan <yoav547@gmail.com>
2023-07-11 10:11:39 +0300

    add commits

--- task08_Yoav_Uzan/src/main.cpp
+++ task08_Yoav_Uzan/src/main.cpp
@@ -5,6 +5,7 @@
 #include <queue>
 #include "message.h"
 using namespace std;
+
 mutex m;
 condition_variable cv;
 std::queue<Message> messages;
@@ -13,59 +14,72 @@ void producer()
 {
 	auto tid = this_thread::get_id();
 	double value = hash<thread::id>{}(tid);
+
+	// Generate messages with decreasing values
 	for (; value > 0; value /= 10.0)
 	{
 		{
 			unique_lock lock(m);
+			// Push the message into the queue
 			messages.push({ tid, value, false });
+			// Notify the consumer that a new message is available
 			cv.notify_one();
 		}
 	}
+
 	{
 		unique_lock<mutex> lock(m);
-		 messages.push({ tid, value, true });
+		// Push a finished message into the queue
+		messages.push({ tid, value, true });
+		// Notify the consumer that a new message is available
 		cv.notify_one();
 	}
 }
 
 void consumer()
 {
-	 int count = 0;
-	 bool flag = true;
-	 Message msg;
+	int count = 0;
+	bool flag = true;
+	Message msg;
 	while (true)
 	{
 		{
 			unique_lock lock(m);
-			cv.wait(lock, []() {return !messages.empty(); });
-			
+			// Wait until there is a message available in the queue
+			cv.wait(lock, []() { return !messages.empty(); });
+
 			if (flag)
 			{
+				// Store the first message for comparison
 				msg = messages.front();
 				flag = false;
 			}
 
 			if (messages.front().finished)
 			{
-				cout << messages.front().tid  << " \033[1;32mFinsh\033[0m" << endl;
+				// Print the finished message ID in green
+				cout << messages.front().tid << " \033[1;32mFinsh\033[0m" << endl;
 				messages.pop();
 				count++;
 			}
 			else
 			{
-				if(msg.tid == messages.front().tid)
+				if (msg.tid == messages.front().tid)
+					// Print the message ID and value in cyan if it matches the first message
 					cout << "\033[1;36m" << messages.front().tid << " sent- " << messages.front().value << "\033[0m\n";
 				else
-					cout <<  "\033[1;31m"<<messages.front().tid << " sent- " << messages.front().value << "\033[0m\n";
+					// Print the message ID and value in red if it doesn't match the first message
+					cout << "\033[1;31m" << messages.front().tid << " sent- " << messages.front().value << "\033[0m\n";
 				messages.pop();
 			}
+
+			// Exit the loop if two finished messages have been processed and the queue is empty
 			if (count == 2 && messages.empty())
 				break;
 		}
-		
+
 	}
 }
-
 
 int main()
 {
@@ -74,7 +88,6 @@ int main()
 		thread t3(consumer);
 		thread t1(producer);
 		thread t2(producer);
-	
 
 		t1.join();
 		t2.join();
@@ -85,5 +98,5 @@ int main()
 		cout << e.what() << endl;
 		return 1;
 	}
-		return 0;
+	return 0;
 }
