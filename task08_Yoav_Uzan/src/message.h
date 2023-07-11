7d83a5b72b72bc2c6db462ab1710cec7defcdfb4
yoav uzan <yoav547@gmail.com>
2023-07-11 10:15:29 +0300

    add commits

--- task08_Yoav_Uzan/src/message.h
+++ task08_Yoav_Uzan/src/message.h
@@ -3,7 +3,7 @@
 using namespace std;
 struct Message
 {
-	thread::id tid;
-	double value = 0;
-	bool finished = false;
+    thread::id tid;    // Thread ID associated with the message
+    double value = 0;  // Value associated with the message
+    bool finished = false;  // Flag indicating if the message is a finished message
 };
